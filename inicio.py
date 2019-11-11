552cd417eb00ff5af73ddb5c325beff40c5ea590
jose <jose-antonio-1110@hotmail.com>
2019-11-11 21:16:11 +0100

    Cambios hechos
    cambios

--- inicio.py
+++ inicio.py
@@ -1,10 +1,4 @@
 print('EEEEEEEEEEEEEEEEEEEEEEE')
 
 print('Aqui estamos')
-<<<<<<< HEAD
-<<<<<<< HEAD
 #EEEEEEEEEEEEEEEEEEEEHHHHH
-=======
->>>>>>> a46438e... primer commit de la practica de EGC
-=======
->>>>>>> a46438e... primer commit de la practica de EGC
