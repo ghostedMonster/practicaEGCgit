29b52330a86b07035d65479bafc56db112f72187
jose <jose-antonio-1110@hotmail.com>
2019-11-11 21:16:11 +0100

    Mas cosas
    vuelta para atras

--- inicio.py
+++ inicio.py
@@ -2,6 +2,9 @@ print('EEEEEEEEEEEEEEEEEEEEEEE')
 
 print('Aqui estamos')
 <<<<<<< HEAD
+<<<<<<< HEAD
 #EEEEEEEEEEEEEEEEEEEEHHHHH
+=======
+>>>>>>> a46438e... primer commit de la practica de EGC
 =======
 >>>>>>> a46438e... primer commit de la practica de EGC
